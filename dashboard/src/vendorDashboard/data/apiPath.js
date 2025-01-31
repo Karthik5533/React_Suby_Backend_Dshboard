618d96b915835e47459252e27ede27671a4edb3a
karthik5533 <karthiknaidu829839@gmail.com>
2025-01-31 18:30:13 -0500

    Changed path to live in api_url

--- dashboard/src/vendorDashboard/data/apiPath.js
+++ dashboard/src/vendorDashboard/data/apiPath.js
@@ -1,5 +1,3 @@
+// export const API_URL = "http://localhost:4000"
 
-export const API_URL = "http://localhost:4000"
-
-//export const API_URL = "https://nodejs-backend-suby-171r.onrender.com"
-
+export const API_URL = "https://nodejs-backend-suby-171r.onrender.com";
